9705faf8e5d9b4fd28989f2998fab7f524ed4736
JivkoJelev91 <zhelev.zhivko.1991@gmail.com>
2019-01-30 09:37:54 +0200

    Add a config file with a secret key

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const request = require('request');
+const config = require('./config') // Make your own config file with your secret key
 const app = express();
 const port = 5000;
 
@@ -23,19 +24,14 @@ app.post('/subscribe', (req, res) => {
             });
     }
 
-    // Secret key
-    const secretKey = '6LcKno0UAAAAAOvOanXu8T0wX3OC49r838kXOEmd';
-
     // Verify URL
     const verifyUrl = `https://google.com/recaptcha/api/
-                       siteverify?secret=${secretKey}&
+                       siteverify?secret=${config.secretKey}&
                        response=${req.body.captcha}&
                        remoteip=${req.connection.remoteAddress}`;
-
     
     // Make request to verify URL
     request(verifyUrl, (err, response, body) => {
-    
 
         // if Not successful
         if(body.success !== undefined && !body.success){
